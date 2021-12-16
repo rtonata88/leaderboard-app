acda190a60e487a3d4eb57e70e87fc4b5b908863
rtonata88 <rtonata@gmail.com>
2021-12-16 20:44:25 +0200

    Add boostrap styling

--- src/index.js
+++ src/index.js
@@ -1,21 +1,24 @@
-import './style.css';
-import { displayScores, recordScore } from './game';
+import "./style.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { displayScores, recordScore } from "./game";
 
 displayScores();
 
 const submitScore = () => {
-  const name = document.getElementById('name');
-  const score = document.getElementById('score');
+  const name = document.getElementById("name");
+  const score = document.getElementById("score");
 
   const data = {
     user: name.value,
     score: score.value,
   };
   recordScore(JSON.stringify(data));
+  name.value = "";
+  score.value = "";
 };
 
-const submitButton = document.getElementById('submitButton');
-submitButton.addEventListener('click', submitScore);
+const submitButton = document.getElementById("submitButton");
+submitButton.addEventListener("click", submitScore);
 
-const refreshButton = document.getElementById('refreshButton');
-refreshButton.addEventListener('click', displayScores);
+const refreshButton = document.getElementById("refreshButton");
+refreshButton.addEventListener("click", displayScores);
